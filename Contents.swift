573b7ae80c06756c36618f5a362c7da683cd61ed
Daniela Becerra <madajiji@gmail.com>
2020-01-15 12:34:37 -0600

    first commit

---
+++ Contents.swift
@@ -0,0 +1,10 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+func shortNameFromName(name: String) -> String {
+    var chars = CharacterSet.init(charactersIn: name)
+    //chars.remove(charactersIn: 0)
+    return String(chards)
+}
